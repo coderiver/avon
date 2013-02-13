6f882ecf267c4ef9e0f4237124a32eefde2b499c
Yevgen <godun.yevgen@gmail.com>
2013-02-13 18:04:19 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+// ---------------- Chose city  drop-list-------------------------- //
     $(".enter__city button").click(function(){
         if ($(this).hasClass("js-act")) {
             $(this).removeClass("js-act");
@@ -9,4 +10,25 @@ $(document).ready(function() {
             $(this).children(".drop-down").fadeIn();
         }
     });
+
+// ----------------  init cycle plugin -------------------------- //
+    $(".coach-scroll .scroll").cycle({ 
+        fx:      'scrollHorz',
+        next:    ".coach-scroll .next", 
+        prev:    ".coach-scroll .prev", 
+        timeout:  0,
+        speed: 700
+    });
+
+    $(".slider .scroll-main").cycle({ 
+        timeout:  0,
+        speed: 1000,
+        pager:  '.slider-nav',
+        pagerAnchorBuilder: function(index, el) {
+            return '<button class=nav-' + index + '><i></i><span></span></button>'; // whatever markup you want
+        }
+    });
+    $(".nav-0 span").text("Тренера");
+    $(".nav-1 span").text("Тренинги");
+    $(".nav-2 span").text("Галерея");
 });
