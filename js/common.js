d4f6f0cb2a12bdd5e4ad4d55e0dfef3c45241e2c
Yevgen <godun.yevgen@gmail.com>
2013-02-13 13:03:26 +0200

    header part1

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-
+    $(".enter__city button").click(function(){
+        if ($(this).hasClass("js-act")) {
+            $(this).removeClass("js-act");
+            $(this).children(".drop-down").fadeOut();
+        }
+        else {
+            $(this).addClass("js-act");
+            $(this).children(".drop-down").fadeIn();
+        }
+    });
 });
