18586bfc3a5b8d7be166af24a8e84617648da420
Yevgen <godun.yevgen@gmail.com>
2013-02-15 16:36:18 +0200

    coach pages

--- js/common.js
+++ js/common.js
@@ -49,11 +49,24 @@ $(document).ready(function() {
         }
     });
 
-// ---------------- close choose city popup -------------------------- //
-$(".popup_choose-city .close").click(function(){
+// ---------------- close popup -------------------------- //
+$(".close").click(function(){
     $(this).parent().fadeOut();
     $(".overlay").fadeOut();
 });
+
+// ---------------- Add comment popup -------------------------- //
+$(".js-add-comment").click(function(){
+    $(".popup_add-comment").fadeIn();
+    $(".overlay").fadeIn();
+});
+
+// ---------------- More -------------------------- //
+    $(".js-more").hide();
+    $(".more div").click(function(){
+        $(this).hide();
+        $(this).parent().prev().slideDown();
+    });
 
 // ---------------- Choose city  drop-list-------------------------- //
     $(".select button").click(function(){
@@ -68,5 +81,20 @@ $(".popup_choose-city .close").click(function(){
             $(this).children(".drop-down").fadeIn();
         }
     });   
+
+// ---------------- Choose city  drop-list-------------------------- //
+$(".switch label").children("span:first").addClass("active");
+$(".switch label").click(function(){
+    if ($(this).children().children("input:checked").length > 0) {
+        $(this).children().children("input").removeAttr("checked");
+        $(this).children("span:first").addClass("active");
+        $(this).children().next().next().removeClass("active");
+    }
+    else {
+        $(this).children().children("input").attr("checked", "checked");
+        $(this).children().removeClass("active");
+        $(this).children().next().next().addClass("active");
+    }
+});
 
 });
